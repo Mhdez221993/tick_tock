24b3d1b40eb125bd58650abf2a57975e13ead941
Moises <mhdezcoronado@gmail.com>
2022-06-15 12:01:50 +0200

    Setup firebase

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -1,1 +1,17 @@
+import firebase from "firebase/app";
+import "firebase/auth";
+import "firebase/firestore";
+import "firebase/storage";
+import firebaseConfig from "lib/firebaseConfig";
 
+const app = !firebase.apps.length ? firebase.initializeApp(firebaseConfig) : firebase.app();
+
+const db = app.firestore();
+const auth = app.auth();
+const storage = app.storage();
+const provider = new firebase.auth.GoogleAuthProvider();
+const serverTimestamp = firebase.firestore.FieldValue.serverTimestamp();
+const increment = firebase.firestore.FieldValue.increment;
+
+export { auth, provider, storage, serverTimestamp, increment };
+export default db;
