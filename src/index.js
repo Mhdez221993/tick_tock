d96b7d058bbb6126a44a510d9c007d531bc6630d
Moises <mhdezcoronado@gmail.com>
2022-11-10 21:33:17 +0100

    Initial code

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import "@draft-js-plugins/mention/lib/plugin.css";
 import AppProviders from "components/AppProviders";
+import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import "./index.scss";
