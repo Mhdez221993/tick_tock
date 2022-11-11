f3d0bc5c0e9ae6f4d592fc94ab163347597791a3
Moises <mhdezcoronado@gmail.com>
2022-11-11 20:59:59 +0100

    Create funtionallity for Signing users

--- src/pages/SignUp.js
+++ src/pages/SignUp.js
@@ -1,10 +1,22 @@
 import db from "lib/firebase";
 import { removeWhiteSpace } from "lib/validate";
 import { useEffect, useState } from "react";
+import toast from "react-hot-toast";
+
+const toastOptions = {
+  position: 'top-right',
+  style: {
+    fontFamily: 'proxima-regular',
+    borderRadius: 10,
+    background: '#333',
+    color: "#fff"
+  }
+}
 
 export default function SignUp({ user }) {
   const [isTaken, setTaken] = useState(false);
   const [username, setUsername] = useState("");
+  const [isLoading, setLoading] = useState(false);
 
   useEffect(() => {
     if (user) {
@@ -31,26 +43,70 @@ export default function SignUp({ user }) {
 
   }, [username])
 
+  async function signUp(e) {
+    e.preventDefault();
+
+    if (isTaken) {
+      return toast.error("This username is taken", toastOptions);
+
+    } else if (username.length < 3) {
+      return toast.error("Username is under 3 character limit", toastOptions);
+
+    } else if (username.length > 20) {
+      return toast.error("Username is over 20 character limit", toastOptions);
+
+    }
+
+    const { uid, displayName, photoURL } = user;
+    const u = `@${removeWhiteSpace(username)}`;
+
+    try {
+      setLoading(true);
+      await db.doc(`users/${uid}`).set({
+        uid,
+        username: u,
+        displayName,
+        photoURL
+      })
+
+    } catch (error) {
+      alert(error);
+
+    } finally {
+      setLoading(false);
+    }
+
+
+  }
+
   return (
     <div className="signup-container">
       <header className="signup-header">
         <signup-header-title>Sign up</signup-header-title>
       </header>
-      <form action="" className="signup-form">
+      <form onSubmit={signUp} className="signup-form">
+
         <div className="signup-inner">
           <div className="signup-form-title">Create username</div>
           <div className={`signup-input ${!isTaken ? "signup-error" : ""}`}>
             <input
               value={username}
-              onChange={e => setUsername(e.target.value)}
+              onChange={(e) => setUsername(e.target.value)}
               type="text"
-              placeholder="Username" />
+              placeholder="Username"
+            />
 
             <div className="signup-error-icon"></div>
           </div>
           <div className="signup-error-text">{isTaken ? "This username is taken." : "You can always change this latter."}</div>
         </div>
-        <button className="signup-submit" disabled={isTaken}>Sign up</button>
+
+        <button
+          type="submit"
+          className="signup-submit"
+          disabled={isTaken | isLoading}
+
+        >Sign up</button>
       </form>
     </div>
   );
