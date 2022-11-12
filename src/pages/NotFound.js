1269a0aaeee9b5b0cbd5f0e4f8af81c209dfd712
Moises <mhdezcoronado@gmail.com>
2022-11-12 21:21:24 +0100

    Create NotFound page

--- src/pages/NotFound.js
+++ src/pages/NotFound.js
@@ -1,3 +1,14 @@
+import NotFoundIcon from "icons/NotFoundIcon";
+
 export default function NotFound() {
-  return "notfound";
+  return (
+    <div className="not-found-container">
+      <div className="not-found-inner">
+        <NotFoundIcon />
+
+        <p className="not-found-title">Page not available</p>
+        <p className="not-found-description">Sorry about that! Please try again later.</p>
+      </div>
+    </div>
+  );
 }
