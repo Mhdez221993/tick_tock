e3c0dda86f411f0a83dbbbcf2140b2248a54536b
Moises <mhdezcoronado@gmail.com>
2022-11-12 20:03:38 +0100

    Make use of context

--- src/context/userContext.js
+++ src/context/userContext.js
@@ -1,5 +1,17 @@
-export function UserProvider() {
-  return "userprovider";
+import { auth } from "lib/firebase";
+import { createContext } from "react";
+import { useAuthState } from "react-firebase-hooks/auth";
+
+const UserContext = createContext();
+
+export function UserProvider({ children }) {
+  const [useAuthUser] = useAuthState(auth);
+
+  return (
+    <UserContext.Provider>
+      {children}
+    </UserContext.Provider>
+  );
 }
 
 export default function useAuthUser() {}
