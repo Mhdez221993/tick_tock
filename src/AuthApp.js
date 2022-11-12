eb878ad28c49c93f7fcf3e8ced23cfe4e47947b6
Moises <mhdezcoronado@gmail.com>
2022-11-12 20:52:07 +0100

    Create Routes

--- src/AuthApp.js
+++ src/AuthApp.js
@@ -1,3 +1,38 @@
+import Header from "components/Header";
+import Feed from "pages/Feed";
+import NotFound from "pages/NotFound";
+import Profile from "pages/Profile";
+import Upload from "pages/Upload";
+import VideoPost from "pages/VideoPost";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+
 export default function AuthApp() {
-  return "authapp";
+  return (
+    <Router>
+      <Header />
+      <Switch>
+
+        <Route exact path="/">
+          <Feed />
+        </Route>
+
+        <Route path="/upload">
+          <Upload />
+        </Route>
+
+        <Route path="/:username/video/:postId">
+          <VideoPost />
+        </Route>
+
+        <Route path="/:username">
+          <Profile />
+        </Route>
+
+        <Route path="*">
+          <NotFound />
+        </Route>
+
+      </Switch>
+    </Router>
+  );
 }
