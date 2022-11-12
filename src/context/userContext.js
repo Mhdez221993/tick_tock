6a27a0a8570a2e7e80568bc2c99b17c29ffa8ce6
Moises <mhdezcoronado@gmail.com>
2022-11-12 20:14:35 +0100

    Pass down the user data with contex

--- src/context/userContext.js
+++ src/context/userContext.js
@@ -1,14 +1,32 @@
-import { auth } from "lib/firebase";
-import { createContext } from "react";
+import db, { auth } from "lib/firebase";
+import { createContext, useEffect, useState } from "react";
 import { useAuthState } from "react-firebase-hooks/auth";
 
 const UserContext = createContext();
 
 export function UserProvider({ children }) {
   const [useAuthUser] = useAuthState(auth);
+  const [user, setUser] = useState(null);
+  const [isLoading, setLoading] = useState(false);
+
+  useEffect(() => {
+    const ref = db.doc(`users/${authUser?.uid}`);
+
+    ref.get().then(doc => {
+      if (doc.exists) {
+        setUser({
+          id: doc.id,
+          ref: doc.ref,
+          ...doc.data()
+        })
+      }
+    }).catch(error => console.error("Error fetching user", error))
+    .finally(() => setLoading(false))
+
+  }, [useAuthUser])
 
   return (
-    <UserContext.Provider>
+    <UserContext.Provider value={[user, isLoading]}>
       {children}
     </UserContext.Provider>
   );
