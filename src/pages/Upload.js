3a482662824b67f6ef3f793f9ab2b14f11f54840
Moises <mhdezcoronado@gmail.com>
2022-11-14 18:26:19 +0100

    Build form to upload

--- src/pages/Upload.js
+++ src/pages/Upload.js
@@ -1,3 +1,4 @@
+import DraftEditor from "components/DraftEditor";
 
 export default function Upload() {
   return (
@@ -25,4 +26,36 @@ export default function Upload() {
   );
 }
 
+function UploadPreview() {
+  return "uploadpreview";
+}
 
+function UploadProgress() {
+  return "uploadprogress";
+}
+
+
+
+function UploadForm() {
+  return (
+    <div className="u-form-container">
+      <div className="u-form-wrapper">
+        <div className="u-form-inner">
+
+          <div className="u-form-header"><span
+          className="u-form-title">Caption</span><span
+          className="u-form-length-container"><span
+          className="u-form-length">0 / 150</span></span></div>
+
+          <div className="u-form-input">
+            <DraftEditor></DraftEditor>
+          </div>
+        </div>
+
+        <div className="u-form-action"><button
+        className="u-form-discard">Discard</button><button
+        className="u-form-submit">Post</button></div>
+      </div>
+    </div>
+  );
+}
