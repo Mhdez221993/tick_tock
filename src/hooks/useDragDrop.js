005b7a36d10c1e9e8689cb7e92540c361667084e
Moises <mhdezcoronado@gmail.com>
2022-11-14 19:06:09 +0100

    Create drag and drop hook

--- src/hooks/useDragDrop.js
+++ src/hooks/useDragDrop.js
@@ -1,1 +1,33 @@
-export default function useDragDrop() {}
+import { useEffect, useRef } from "react";
+
+export default function useDragDrop() {
+  const dropRef = useRef();
+
+  function handleDrag(e){
+    e.preventDefault();
+    e.stopPropagation();
+  }
+
+  function handleDrop(e){
+    e.preventDefault();
+    e.stopPropagation();
+
+    if (e.dataTransfer.files && e.dataTransfer.files.length > 0) {
+      const file = e.dataTransfer.files[0];
+      console.log(file);
+    }
+  }
+
+  useEffect(() => {
+    const dropErea = dropRef.current;
+    dropErea.addEventListener('dragover', handleDrag);
+    dropErea.addEventListener('drop', handleDrop);
+
+    return () => {
+      dropErea.removeEventListener('dragover', handleDrag);
+      dropErea.removeEventListener('drop', handleDrop);
+    }
+  })
+
+  return { dropRef };
+}
