2ef3b89583e2dbc2c5585e0214ba0970afb761af
Moises <mhdezcoronado@gmail.com>
2022-11-15 19:28:20 +0100

    Save the file locally while is uploading

--- src/hooks/useFirebaseUpload.js
+++ src/hooks/useFirebaseUpload.js
@@ -1,1 +1,16 @@
-export default function useFirebaseUpload() {}
+import { useState } from "react";
+
+export default function useFirebaseUpload(user) {
+  const [file, setFile] = useState(null);
+  const [uploadTask, setUploadTask] = useState(null);
+  const [isUploading, setUploading] = useState(false);
+  const [uploadProgress, setUploadProgress] = useState(0);
+  const [videoUrl, setVideoUrl] = useState('');
+
+  function handleUpload(file) {
+    setFile(file);
+    setUploading(true);
+  }
+
+  return { handleUpload };
+}
