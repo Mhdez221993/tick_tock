73e58aae54b215017da49ddbd77061ff05ab7161
Moises <mhdezcoronado@gmail.com>
2022-11-16 18:27:06 +0100

    Create useDiscardModal context

--- src/context/discardModalContext.js
+++ src/context/discardModalContext.js
@@ -1,14 +1,25 @@
-import { createContext } from "react";
+import { createContext, useContext, useState } from "react";
 
 const DiscardModalContext = createContext();
 
 export function DiscardModalProvider({ children }) {
+  const [isDiscardOpen, setDiscardOpen] = useState(false);
+  const closeDiscard = () => setDiscardOpen(false);
+  const openDiscard = () => setDiscardOpen(true);
 
   return (
-    <DiscardModalContext.Provider>
+    <DiscardModalContext.Provider value={{ isDiscardOpen, closeDiscard, openDiscard }}>
       { children }
     </DiscardModalContext.Provider>
   );
 }
 
-export default function useDiscardModal() {}
+export default function useDiscardModal() {
+  const context = useContext(DiscardModalContext);
+
+  if (context === undefined) {
+    throw new Error("useDiscardModal must be used with DiscardModalContext.Provider")
+  }
+
+  return context;
+}
