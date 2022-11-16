5d35e751928e079c486ee9aa3189cee5ca77b852
Moises <mhdezcoronado@gmail.com>
2022-11-16 18:02:31 +0100

    Create DiscardModal conponent

--- src/components/DiscardModal.js
+++ src/components/DiscardModal.js
@@ -1,3 +1,19 @@
+import Modal from "components/Modal";
+
 export default function DiscardModal() {
-  return "discardmodal";
+  return (
+    <Modal>
+      <div className="discard-modal-container">
+        <div className="discard-modal-title-container">
+          <div className="discard-modal-title">Discard this post?</div>
+          <div className="discard-modal-subtitle">
+            The video and all edits will be discarded
+          </div>
+        </div>
+
+        <button className="discard-modal-confirm">Discard</button>
+        <button className="discard-modal-discard">Continue editing</button>
+      </div>
+    </Modal>
+  );
 }
