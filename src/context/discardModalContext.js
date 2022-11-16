52118350490c65d56a61d277ace9276a45b4ca84
Moises <mhdezcoronado@gmail.com>
2022-11-16 18:07:05 +0100

    Build discard modal context

--- src/context/discardModalContext.js
+++ src/context/discardModalContext.js
@@ -1,5 +1,14 @@
-export function DiscardModalProvider() {
-  return "discardmodalprovider";
+import { createContext } from "react";
+
+const DiscardModalContext = createContext();
+
+export function DiscardModalProvider({ children }) {
+
+  return (
+    <DiscardModalContext.Provider>
+      { children }
+    </DiscardModalContext.Provider>
+  );
 }
 
 export default function useDiscardModal() {}
